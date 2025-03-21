8d6ae31e55bb1610bf2a92b78a93d7c9c7e93ee2
Trevor Amestoy <89143989+TrevorJA@users.noreply.github.com>
2025-03-21 13:53:27 -0400

    Edit load_observations to keep df format

--- methods/load/observations.py
+++ methods/load/observations.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 def load_observations(datatype,
                       reservoir_name=None,
-                      data_dir = "./data/"):
+                      data_dir = "./data/",
+                      as_numpy=True):
     """
     Loads observational data (inflow, storage or release).
     
@@ -18,12 +19,23 @@ def load_observations(datatype,
     
     filepath = f"{data_dir}{datatype}.csv"
 
-    df = pd.read_csv(filepath)
+    df = pd.read_csv(filepath, index_col = 0, parse_dates=True)
+    df.index = pd.to_datetime(df.index.date)
+    
+    
+    # set 0.0 to NaN
+    df = df.replace(0.0, pd.NA)
     
     if (reservoir_name is not None) and (reservoir_name not in df.columns):
         raise ValueError(f"Reservoir '{reservoir_name}' not found in {filepath}. Check CSV headers.")
     
     if reservoir_name is None:
-        return df.to_numpy()  # Return the whole DataFrame as a numpy array
+        if not as_numpy:
+            return df
+        else:
+            return df.to_numpy()  # Return the whole DataFrame as a numpy array
     else:
-        return df[reservoir_name].values  # Return the specified reservoir's data as a numpy array
+        if not as_numpy:
+            return df[[reservoir_name]]
+        else:        
+            return df[reservoir_name].values  # Return the specified reservoir's data as a numpy array
